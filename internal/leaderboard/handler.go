752763b15eb47e5529badb32733f2b20a48e2c48
Krishanu Saha <krishanu1137@gmail.com>
2025-05-30 08:06:20 +0530

    Leaderboard added

---
+++ internal/leaderboard/handler.go
@@ -0,0 +1,30 @@
+package leaderboard
+
+import (
+	"encoding/json"
+	"net/http"
+	"strconv"
+)
+
+type Handler struct {
+	service *Service
+}
+
+func NewHandler(service *Service) *Handler {
+	return &Handler{service: service}
+}
+
+func (h *Handler) GetLeaderboard(w http.ResponseWriter, r *http.Request) {
+	limitStr := r.URL.Query().Get("limit")
+	limit, err := strconv.Atoi(limitStr)
+	if err != nil || limit <= 0 {
+		limit = 10
+	}
+	leaderboard, err := h.service.GetLeaderboard(limit)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(leaderboard)
+}
